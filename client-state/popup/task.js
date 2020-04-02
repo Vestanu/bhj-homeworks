0c07cf9cdc00250ed2fa4b55fc33e099420f410c
Tatiana Penkova <tatianapenkova@gmail.com>
2020-04-02 20:38:40 +0300

    3 задание

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,16 +1,19 @@
+
+let subscribeElement = document.getElementById("subscribe-modal");
+
 let showSubscribe = function() {
-    if (document.cookie.replace( /(?:(?:^|.*;\s*)subcribeShown\s*\=\s*([^;]*).*$)|^.*$/, "$1" ) !== "true") {
-  
-      let subscribeElement = document.getElementById("subscribe-modal");
+    if (document.cookie.replace(/(?:(?:^|.*;\s*)closeModal\s*\=\s*([^;]*).*$)|^.*$/, "$1" ) !== true) {
+      
       subscribeElement.classList.add("modal_active");
-  
+    }
       subscribeElement.querySelector(".modal__close").addEventListener("click", function(e) {
         this.closest(".modal").classList.remove("modal_active");
-        document.cookie =
-        "doSomethingOnlyOnce=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+        // alert("Do something here!");
+        document.cookie = "closeModal = true; expires=Fri, 24 Dec 2020 23:59:59 GMT";
+        console.log(document.cookie)
       });
   
-    }
+    
   };
   
   showSubscribe();
